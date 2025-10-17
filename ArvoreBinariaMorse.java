641a4bc4177cf6aa217f6b737864a85c94daf534
Brenda Barbosa <151410709+brebarbosaa@users.noreply.github.com>
2025-10-17 13:35:30 -0300

    Métodos buscar e exibir

--- ArvoreBinariaMorse.java
+++ ArvoreBinariaMorse.java
@@ -29,4 +29,65 @@ public class ArvoreBinariaMorse {
         atual.caractere = caractere;
     }
 
+    // percorre a árvore e retorna o caractere encontrado conforme código morse
+    char buscarLetra(String codigo){
+        Nodo atual = raiz;
+        for (int i = 0; i < codigo.length(); i++){
+            char simbolo = codigo.charAt(i);
+            if (simbolo == '.') {
+                if (atual.esquerda == null)
+                    return '?'; // simbolo não encontrado
+                atual = atual.esquerda;
+            } else if (simbolo == '-'){
+                if (atual.direita == null)
+                    return '?'; // não encontrado
+                atual = atual.direita;
+            }
+
+        }
+        return atual.caractere;
+    }
+
+    // retornar mensagem montada
+    String buscar(String mensagem){
+        String resultado = "";
+        String codigo = "";
+
+        for (int i = 0; i < mensagem.length(); i++){
+            char c = mensagem.charAt(i);
+
+            if (c != ' ') {
+                codigo = codigo + c; // formação do código
+            } else {
+                if (!codigo.isEmpty()){
+                    resultado = resultado + buscarLetra(codigo);
+                    codigo = "";
+                }
+            }
+        }
+
+        if (!codigo.isEmpty())
+            resultado = resultado + buscarLetra(codigo);
+        return resultado;
+
+    }
+
+    void exibirArvore(){
+        exibirArvoreRec(raiz, 0);
+    }
+
+    void exibirArvoreRec(Nodo no, int nivel){
+        if (no == null)
+            return;
+
+        // imprime primeiro a direita e depois a esquerda
+        exibirArvoreRec(no.direita, nivel+ 1);
+
+        for (int i = 0; i < nivel; i++)
+            System.out.println("  ");
+        System.out.println(no.caractere);
+
+        exibirArvoreRec(no.esquerda, nivel + 1);
+    }
+
 }
--- Main.java
+++ Main.java
@@ -7,5 +7,13 @@ public class Main {
         arvore.inserir("...", 'S'); //exemplo de uso do documento
         arvore.inserir("---",'O');
 
+        System.out.println("buscando '...': " + arvore.buscar("..."));
+        System.out.println("buscando '---': " + arvore.buscar("---"));
+
+        // imprime a árvore:
+
+        System.out.println("\nÁrvore binária morse: ");
+        arvore.exibirArvore();
+
     }
 }
